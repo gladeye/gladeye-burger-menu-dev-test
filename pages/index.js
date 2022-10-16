0532cc23c433d85873bb0e3b1348f17cf03d50d1
Michael Davies <michaeldavies@Michaels-MacBook-Pro.local>
2022-10-17 11:47:48 +1300

    chore: add prettier
    Signed-off-by: Michael Davies <michaeldavies@Michaels-MacBook-Pro.local>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -14,5 +14,5 @@ export default function Home() {
         <h1>Gladeye Burger Menu Dev Test</h1>
       </main>
     </div>
-  )
+  );
 }
